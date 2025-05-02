52876c36ae8376206a6a007c4e9aa75662845eee
KillDare <87498894+KillDare@users.noreply.github.com>
2025-05-02 15:46:06 -0300

    Update and rename Análise sinal sEMG.ipynb to Análise sinal sEMG.py

--- Análise sinal sEMG.py
+++ Análise sinal sEMG.py
@@ -1,10 +1,3 @@
-echo "# Analise-EMG" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/KillDare/Analise-EMG.git
-git push -u origin main
 
 fd = 2000; # frequenncia da amostragem em Hz
 inicioMoviento = 0; # indica o instante inicio do movimento em segundos
