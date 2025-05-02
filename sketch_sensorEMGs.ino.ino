175e0a4c78c96d6ad001aa1a85a8ba1e9b57d88b
KillDare <87498894+KillDare@users.noreply.github.com>
2025-05-02 15:47:32 -0300

    Add files via upload

---
+++ sketch_sensorEMGs.ino.ino
@@ -0,0 +1,17 @@
+float tensao;
+int cont;
+
+void setup() {
+    Serial.begin(19200);  // Inicia a comunicação serial
+    tensao = 0;
+    cont = 0;
+}
+
+void loop() {
+    cont++;
+    int valorEMG = analogRead(A0);  // Lê o valor do sensor EMG
+    tensao =  ((valorEMG / 1023.0) * 5.0);
+    Serial.print(tensao);
+    Serial.print(" ");
+    delay(10);  // Pequeno atraso para evitar sobrecarga
+}
